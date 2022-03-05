55b7aa0f1baaa62ebd0b999c6906c1fa3a649772
felicity <felicityabel2016@gmail.com>
2022-03-05 20:36:11 +0100

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAPP4Gq9K57S6xz_zP5j_ZYBw63Bw5Zgr8",
-  authDomain: "botchi-web.firebaseapp.com",
-  projectId: "botchi-web",
-  storageBucket: "botchi-web.appspot.com",
-  messagingSenderId: "49501436207",
-  appId: "1:49501436207:web:60407f49fd4297dd8049da",
-  measurementId: "G-2WECCPWCG0"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// const db = getFirestore(app)
-// firebaseConfig.initializeApp(firebaseConfig)
-var firebase =  firebase.firebase()
-// reference database
-firebase.database().ref('contactForm')
+  
+// Headers
+// about
+// download
+// services details
+// how it ServiceWorkerRegistration
+// download
+// keypoint
+// contact us
+// footer
